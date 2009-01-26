905c7fa01b2f8594a2d339be8018acd3ef2e4c90
Chris J. Davis <cjdavis@viewzi.com>
2009-01-26 03:23:10 +0000

    new paypal code
    git-svn-id: http://chrisjdavis.svn.beanstalkapp.com/work@17 131f031f-3a30-468c-80f7-b2844c79c7db

--- trunk/wicketpixie/app/advert.php
+++ trunk/wicketpixie/app/advert.php
@@ -1,6 +1,10 @@
 <div id="admin-affiliates">
 	<h3>Donate to WicketPixie</h3>					
-	Waiting for new Paypal form code.
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">  
+		<input type="hidden" name="cmd" value="_s-xclick"> 
+		<input type="hidden" name="hosted_button_id" value="2767322"> 
+		<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt=""> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+	</form>
 	<?php
 	$feeds= array(
 		array( 'feed' => 'http://feeds.pirillo.com/WicketPixieRecommendations',
