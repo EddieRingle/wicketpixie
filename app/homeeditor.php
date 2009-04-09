27c22db1959f3832bb9bc0bbb2928bbf641dea95
Eddie Ringle <eddie@eringle.net>
2009-04-08 22:37:19 -0400

    Too many changes to describe here. Major change? _buggy_ Home Editor.

---
+++ app/homeeditor.php
@@ -0,0 +1,198 @@
+<?php
+/**
+* WicketPixie Home Editor
+* Now you finally do not have to dig into the template files to
+* modify your home page! Whee! \o/
+**/
+
+// Arrays that hold our options
+$homeoptions = array(
+    array(
+    "name" => "Flickr Widget",
+    "description" => "Display the Flickr widget on the home page.",
+    "id" => "home_flickr",
+    "std" => 1,
+    "status" => "checked",
+    "type" => "checkbox"),
+    array(
+    "name" => "Video Embed",
+    "description" => "Enable the Video Embed Code entered below.",
+    "id" => "home_video",
+    "std" => 1,
+    "status" => "checked",
+    "type" => "checkbox"),
+    array(
+    "name" => "Show 'My Videos' heading",
+    "description" => "Show the 'My Videos' heading above the video embed.",
+    "id" => "home_show_vid_heading",
+    "std" => 1,
+    "status" => "checked",
+    "type" => "checkbox"),
+    array(
+    "name" => "Show 'Recent Photos' heading",
+    "description" => "Show the 'Recent Photos' heading above the video embed.",
+    "id" => "home_show_photo_heading",
+    "std" => 1,
+    "status" => "checked",
+    "type" => "checkbox"),
+    array(
+    "name" => "Video Embed Code",
+    "description" => "Enter code for a video object. For example, a YouTube custom player.",
+    "id" => "home_video_code",
+    "std" => "No video embed code yet...",
+    "type" => "textarea"),
+    array(
+    "name" => "Custom Code",
+    "description" => "Content that is displayed after the post but before the Flickr Widget, Embedded Video, etc.",
+    "id" => "home_custom",
+    "std" => "No custom code yet...",
+    "type" => "textarea")
+);
+class HomeAdmin {
+
+	/**
+	* Just calling WP's method to add a new menu to the design section.
+	*/
+	function addMenu()
+	{
+	    global $homeoptions;
+        if ( 'save' == $_POST['action'] ) {
+            check_admin_referer('wicketpixie-settings');
+            foreach ( $homeoptions as $value ) {
+	            if(wp_get_option($value['id'])) {
+				    wp_update_option( $value['id'], $_POST[ $value['id'] ] );
+				} else {
+				    if(wp_option_isempty($value['id']) == true) {
+				        wp_update_option($value['id'],$_POST[$value['id']]);
+				    } else {
+				        wp_add_option($value['id'],$_POST[$value['id']]);
+				    }
+				}
+			}
+            foreach ( $homeoptions as $value ) {
+				if( isset( $_POST[ $value['id'] ] ) ) { 
+					if( $value['type'] == 'checkbox' ) {
+						if( $value['status'] == 'checked' ) {
+						    if(wp_get_option($value['id'])) {
+							    wp_update_option( $value['id'], '1' );
+							} else {
+							    if(wp_option_isempty($value['id']) == true) {
+							        wp_update_option($value['id'],'1');
+							    } else {
+							        wp_add_option($value['id'], '1');
+							    }
+							}
+						} else {
+						    if(wp_get_option($value['id'])) {
+							    wp_update_option( $value['id'], '0' );
+							} else {
+							    if(wp_option_isempty($value['id']) == true) {
+							        wp_update_option($value['id'],'0');
+							    } else {
+							        wp_add_option($value['id'], '0');
+							    }
+							}
+						}
+					} elseif( $value['type'] != 'checkbox' ) {
+					    if(wp_get_option($value['id'])) {
+						    wp_update_option( $value['id'], $_POST[ $value['id'] ]  );
+						} else {
+						    wp_add_option($value['id'],$_POST[$value['id']]);
+						}
+					} else {
+						if(wp_get_option($value['id'])) {
+						    wp_update_option( $value['id'], $_POST[ $value['id'] ]  );
+						} else {
+						    wp_add_option($value['id'],$_POST[$value['id']]);
+						}
+					}
+				}
+			}
+            wp_redirect($_SERVER['PHP_SELF'] ."?page=homeeditor.php&saved=true");
+            die;
+	    }
+		add_submenu_page('wicketpixie-admin.php', __('WicketPixie Home Editor'), __('Home Editor'), 9, basename(__FILE__), array( 'HomeAdmin', 'homeMenu' ) );
+	}
+
+	/**
+	* The admin page for our home editor.
+	**/
+    function homeMenu()
+    {
+        global $homeoptions;
+        
+        if ( isset( $_REQUEST['saved'] ) ) echo '<div id="message" class="updated fade"><p><strong>'.__('Options saved.').'</strong></p></div>';
+    ?>
+    <div class="wrap">
+	
+	    <div id="admin-options">
+	
+		    <h2>Home Editor</h2>
+            
+		    <form method="post" style="padding:20px 0 10px;" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=homeeditor.php&amp;saved=true">
+                <?php wp_nonce_field('wicketpixie-settings'); ?>
+			    <table class="form-table">
+
+			    <?php foreach ($homeoptions as $value) {
+        
+			    if ($value['type'] == "textarea") { ?>
+            
+			    <tr valign="top"> 
+			        <th scope="row" style="font-size:12px; text-align:left; padding-right:10px;"><acronym title="<?php echo $value['description']; ?>"><?php echo $value['name']; ?></acronym></th>
+			        <td style="padding-bottom:10px;">
+			            <textarea name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" value="<?php if ( wp_get_option( $value['id'] ) != false && wp_get_option($value['id']) != '') { echo wp_get_option( $value['id'] ); } else { echo $value['std']; } ?>"><?php (wp_get_option($value['id']) != false && wp_get_option($value['id']) != '') ? wp_get_option($value['id']) : $value['std']; ?></textarea>
+			        </td>
+			    </tr>
+
+			    <?php } elseif ($value['type'] == "select") { ?>
+
+			        <tr valign="top"> 
+			            <th scope="row" style="font-size:12px; text-align:left; padding-right:10px;"><acronym title="<?php echo $value['description']; ?>"><?php echo $value['name']; ?></acronym></th>
+			            <td style="padding-bottom:10px;">
+			                <select name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>">
+			                    <?php foreach ($value['options'] as $option) { ?>
+			                    <option<?php
+								    if ( $option == wp_get_option( $value['id'] ) ) { 
+									    echo ' selected="selected"'; 
+								    } elseif( $option == $value['std'] && !wp_get_option( $value['id'] ) ) {
+									    echo ' selected="selected"';
+								    } ?>
+							    ><?php echo $option; ?></option>
+			                    <?php } ?>
+			                </select>
+			            </td>
+			        </tr>
+
+			    <?php 
+				    } elseif( $value['type'] == 'checkbox' ) { ?>
+				    <tr valign="top">
+				        <th scope="row" style="font-size:12px; text-align:left; padding-right:10px;"><acronym title="<?php echo $value['description']; ?>"><?php echo $value['name']; ?></acronym></th>
+			            <td style="padding-right:10px;">
+					        <?php
+						        if (wp_get_option($value['id']) != false) {
+							        $checked = wp_get_option($value['id']);
+						        } else { 
+							        $checked = $value['std']; 
+						        }
+					        ?>
+					        <input name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php echo $value['id']; ?>" <?php if($checked === '1') { echo "checked='checked'"; } ?> />
+					</tr>
+			    <?php
+				    }
+			    }
+			    ?>
+
+			    </table>
+
+			    <p class="submit">
+				    <input name="save" type="submit" value="Save changes" class="button" />    
+				    <input type="hidden" name="action" value="save" />
+			    </p>
+
+		    </form>
+	    </div>
+	    <?php include_once('advert.php'); ?>
+    <?php
+    }
+}
+?>
--- wicketpixie-admin.php
+++ wicketpixie-admin.php
@@ -5,6 +5,7 @@ $settings = array(
         "name" => "Blog Feed URL",
         "description" => "RSS feed URL of your blog.",
         "id" => "blog_feed_url",
+        "std" => "http://feeds.pirillo.com/ChrisPirillo",
         "type" => "text"),
     array(
         "name" => "Flickr ID",
@@ -15,54 +16,123 @@ $settings = array(
         "name" => "Podcast Feed URL",
         "description" => "URL of your podcast's feed.",
         "id" => "podcastfeed",
+        "std" => "http://feeds.pirillo.com/ChrisPirilloShow",
         "type" => "text"),
     array(
         "name" => "Twitter Username",
         "description" => "Twitter Username",
         "id" => "twitterid",
+        "std" => "chrispirillo",
         "type" => "text"),
     array(
         "name" => "Ustream Channel",
         "description" => "Channel name of stream set for live video.",
         "id" => "ustreamchannel",
+        "std" => "chris-pirillo-live",
         "type" => "text"),
     array(
         "name" => "YouTube Username",
         "description" => "Your username for YouTube.",
         "id" => "youtubeid",
-        "type" => "text")
+        "std" => "lockergnome",
+        "type" => "text"),
+    	array(
+		"name"	=>	"Show author on posts",
+		"description"	=>	"Whether or not to show who wrote a particular post.",
+		"id"	=> "auth_credit",
+		"std"	=>	1,
+		"status" => 'checked',
+		"type"	=>	'checkbox'),	
+    array(
+        "name"  =>  "Enable WicketPixie Notifications",
+        "description"   => "Check this if you want WicketPixie to notify services like Ping.fm about your new blog posts, as configured on the WicketPixie Notifications page.",
+		"id"    =>  "notify",
+        "std"   =>  1,
+        	"status" => 'checked',
+        "type"  => 'checkbox'),
+    array(
+        "name"  =>  "Display topbar",
+        "description"   => "Check this if you want the hovering black bar to appear on the top of your blog.",
+        "id"    => "show_topbar",
+        "std"   => 1,
+        "status" => 'checked',
+        "type"  =>  'checkbox')
 );
 
-function save($data) {
-    global $settings;
-    foreach($settings as $setting) {
-        $name = $setting['name'];
-        $id = $setting['id'];
-        $type = $setting['type'];
-        if($type == "text") {
-            if($_POST[$id] != $name) {
-                $value = $_POST[$id];
-                if(!wp_get_option("wp_$id")) {
-                    wp_add_option("wp_$id",$value);
-                } else {
-                    wp_update_option("wp_$id",$value);
+function save($data,$array) {
+    check_admin_referer('wicketpixie-settings');
+            foreach ( $array as $value ) {
+                if(wp_get_option($value['id'])) {
+				    wp_update_option( $value['id'], $_POST[ $value['id'] ] );
+				} else {
+				    if(wp_option_isempty($value['id']) == true) {
+				        wp_update_option($value['id'],$_POST[$value['id']]);
+				    } else {
+				        wp_add_option($value['id'],$_POST[$value['id']]);
+				    }
+				}
+            }
+            foreach ( $array as $value ) {
+                if( isset( $_POST[ $value['id'] ] ) ) { 
+                    if( $value['type'] == 'checkbox' ) {
+                        if( $value['status'] == 'checked' ) {
+                            if(wp_get_option($value['id'])) {
+				                wp_update_option( $value['id'], '1');
+				            } else {
+				                if(wp_option_isempty($value['id']) == true) {
+				                    wp_update_option($value['id'],'1');
+				                } else {
+				                    wp_add_option($value['id'],'1');
+				                }
+				            }
+                        } else {
+                            if(wp_get_option($value['id'])) {
+				                wp_update_option( $value['id'], '0');
+				            } else {
+				                if(wp_option_isempty($value['id']) == true) {
+				                    wp_update_option($value['id'],'0');
+				                } else {
+				                    wp_add_option($value['id'],'0');
+				                }
+				            }
+                        }	
+                    } elseif( $value['type'] != 'checkbox' ) {
+                        if(wp_get_option($value['id'])) {
+				            wp_update_option( $value['id'], $_POST[ $value['id'] ] );
+				        } else {
+				        if(wp_option_isempty($value['id']) == true) {
+				            wp_update_option($value['id'],$_POST[$value['id']]);
+				        } else {
+				            wp_add_option($value['id'],$_POST[$value['id']]);
+				        }
+				    }
+                    } else {
+                        if(wp_get_option($value['id'])) {
+				            wp_update_option( $value['id'], $_POST[ $value['id'] ] );
+				        } else {
+				            if(wp_option_isempty($value['id']) == true) {
+				                wp_update_option($value['id'],$_POST[$value['id']]);
+				            } else {
+				                wp_add_option($value['id'],$_POST[$value['id']]);
+				            }
+				        }
+                    }
                 }
             }
-        } elseif($type == "checkbox") {
-            // todo
-        }
-    }
+            wp_redirect($_SERVER['PHP_SELF'] ."?page=".basename(__FILE__)."&saved=true");
+            die;
 }
 
 function wicketpixie_toplevel_admin() {
+    global $settings;
+    if($_POST['action'] == 'save_settings') {
+        save($_POST,$settings);
+    }
 add_menu_page('WicketPixie Admin', 'WicketPixie', 'edit_themes', 'wicketpixie-admin.php', 'wicketpixie_admin_index',get_template_directory_uri() .'/images/wicketsmall.png');
 }
 
 function wicketpixie_admin_index() {
     global $settings;
-    if($_POST['idform'] == 'true') {
-        save($_POST);
-    }
 ?>
 			<div class="wrap">
 				
@@ -75,33 +145,31 @@ function wicketpixie_admin_index() {
                     <p id="flickrid_tip">You can obtain your Flickr ID by using <a href="http://idgettr.com">idGettr</a>.</p>
                     <p id="ustreamchannel_tip">Your Ustream Channel is the name of the Ustream channel you'd like to stream from. For example, the channel 'Chris Pirillo Live' (url of which is http://ustream.tv/channel/chris-pirillo-live) would be entered as 'chris-pirillo-live'. (Like you'd see it in the Ustream URL.)</p>
 		            <form method="post" style="padding:20px 0 40px;" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=wicketpixie-admin.php">
+		            <?php wp_nonce_field('wicketpixie-settings'); ?>
 		            <table class="form-table">
-			            <?php foreach( $settings as $setting ) {
-			            $id = $setting['id'];
-                        $name = $setting['name'];
-                        $desc = $setting['description'];
-                        $optdata = wp_get_option("wp_$id");
-                        if($optdata == false || $optdata == "") {
-                            $value = "";
-                        } else {
-                            $value = $optdata;
-                        }
-                        ?>
+			            <?php foreach( $settings as $value ) { ?>
 			            <tr valign="top"> 
-				            <th scope="row" style="font-size:12px;text-align:left;padding-right:10px;">
-					            <acronym title="<?php echo $desc; ?>"><?php echo $name; ?></acronym>
-				            </th>
-				            <td style="padding-right:10px;">
-					            <input name="<?php echo $id; ?>" id="<?php echo $id; ?>" type="text" value="<?php echo $value; ?>" />
-				            </td>
-                        </tr>
-                        <?php } ?>
-                    </table>
-                        <p class="submit">
-                            <input type="submit" name="submit" value="Save" />
-                            <input type="hidden" name="idform" value="true" />
-                        </p>
-                    </form>
+				<th scope="row" style="font-size:12px;text-align:left;padding-right:10px;">
+					<acronym title="<?php echo $value['description']; ?>"><?php echo $value['name']; ?></acronym>
+				</th>
+				<td style="padding-right:10px;">
+					<?php
+						if (wp_get_option($value['id']) != false) {
+							$optdata = wp_get_option($value['id']);
+						} else { 
+							$optdata = $value['std']; 
+						}
+					?>
+					<input name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php echo $optdata; ?>" <?php if($optdata === '1') { echo "checked='checked'"; } ?> />
+				</td>
+			</tr>
+			<?php } ?>
+		</table>
+			<p class="submit">
+			<input name="save_settings" type="submit" value="Save changes" />    
+			<input type="hidden" name="action" value="save_settings" />
+			</p>
+		</form>
                 </div>
                 <?php include_once('app/advert.php'); ?>
 <?php
