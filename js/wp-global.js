01dc5fe3ba59c513b23dc26c820c93ed508f33fa
dabates <me@dave-bates.net>
2009-04-13 10:44:14 -0500

    Fixed the disqus things.. Remove the "Comments"

--- js/wp-global.js
+++ js/wp-global.js
@@ -1,21 +1,8 @@
 $(document).ready(function() {
-    var links = document.getElementsByTagName('a');
-    var num_replies = ''.split(',');
-    
-    alert ("Links: " + links.length);
-    
-    for (var i = 0, j=0; i < links.length; i++) {
-        if (links[i].indexOf('#disqus_thread') >= 0) {
-            
-            count = parseInt(num_replies[j]);
-            if (count != undefined && !isNaN(count)) {
-                if (count > 1) { links[i].innertHTML = count; }
-                else if (!count) { links[i].innerHTML = "0"; }
-                else { links[i].innerHTML = "1"; }
-            }
-            j++;
-        }
-    }
+	$('a').each(function (idx) {
+		if ($(this).attr('href').indexOf('#disqus_thread') >= 0) {
+			count = parseInt($(this).html());
+			$(this).html(count);
+		}
+	});
 });
-
-//alert("Loaded File");
