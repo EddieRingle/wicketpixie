e627a1d83000c11bd1b20c59b49da92686c42121
dabates <me@dave-bates.net>
2009-04-17 16:44:29 -0500

    Added a ReadyScript for to keep the background white until the page is loaded

---
+++ js/wp-global.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    //Add all your global onReady Functions here
+    
+    safeLoading();
+    
+});
+
+function safeLoading() {
+    $("#mid").css("background-color","transparent")
+        .css("background-image","url(../images/mid-bg.jpg)")
+        .css("background-repeat", "no-repeat")
+        .css("background-position", "0 0");
+}
