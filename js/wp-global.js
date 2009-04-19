bd80949bffed2cd67f61cea60b18399afd8c394c
Eddie Ringle <eddie@eringle.net>
2009-04-19 11:27:53 -0400

    Almost done with background color fix.

--- js/wp-global.js
+++ js/wp-global.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     //Add all your global onReady Functions here
-    //safeLoading();
+    setTimeout("safeLoading()",2500);
 });
 
 function safeLoading() {
-    $("#mid").css("background-color","transparent")
-        .css("background-image","url(../images/mid-bg.jpg)")
-        .css("background-repeat", "no-repeat")
-        .css("background-position", "0 0");
+    $("#mid").css("background-color","transparent");
+//        .css("background-image","url(../images/mid-bg.jpg)")
+//        .css("background-repeat", "no-repeat")
+//        .css("background-position", "0 0");
 }
