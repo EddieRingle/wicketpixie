2c45a958076e9a67485e39678a00d01286d59aab
dabates <me@dave-bates.net>
2009-05-13 02:16:19 +0800

    Forgot to commit js script
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- js/wp-global.js
+++ js/wp-global.js
@@ -1,28 +1,9 @@
 $(document).ready(function () {
     //Add all your global onReady Functions here
-    setTimeout("replaceCSS_navbar()",300);
-    //setTimeout("replaceCSS_mid()",1000);
-    setTimeout("replaceCSS_prewrapper()",300);
-    setTimeout("replaceCSS_wrapper()",350);
-    setTimeout("replaceCSS_sidebar()",300);
+    setTimeout("hideLoading()", 500);
 });
 
-function replaceCSS_navbar() {
-    $("#navLoading").css("display","none")
-        .css("background-color", "transparent");
-}
 
-function replaceCSS_sidebar() {
-    $("#sidebar").css("background-color","none");
-}
-
-function replaceCSS_prewrapper() {
-    $("#wrapper").css("padding","0 20px");
-}
-function replaceCSS_wrapper() {
-    $("#wrapper").css("background-color","transparent");
-}
-
-function replaceCSS_mid() {
-    $("#mid").css("background-color","transparent");
+function hideLoading() {
+    $("#loadingFrame").css('display', 'none');	
 }
