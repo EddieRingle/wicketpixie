b2998f1a616b88054292242993ad627e575f0d1c
Eddie Ringle <eddie@eringle.net>
2009-08-29 14:26:29 -0400

    Removed unnecessary stuff from the admin pages' sidebar.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- app/advert.php
+++ app/advert.php
@@ -5,42 +5,4 @@
 		<input type="hidden" name="hosted_button_id" value="2767322"> 
 		<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt=""> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 	</form>
-    <h3>Subscribe to Chris Pirillo's Newsletter!</h3>
-    <form action="http://whatcounts.com/bin/listctrl" method="post">
-        <input type=hidden name="slid" value="4EB045FEF6973258752D42129F9F915C" />
-        <input type=hidden name="cmd" value="subscribe" />
-        <input type=hidden name="goto" value="http://chris.pirillo.com/" />
-        <input type=hidden name="key" value="slid" />
-        E-Mail: <input type=text name=email size=13 />
-        <input type=hidden name="format" value="plain" />
-        <input type=submit value="Subscribe" />
-    </form>
-    <h3>Chris Pirillo Uses and Recommends...</h3>
-    <ul>
-        <li><a href="http://chitika.com/publishers.php?refid=lockergnome&from=mm" rel="nofollow">Chitika for Shopping Banners</a></li>
-        <li><a href="http://www.geckotribe.com/rss/carp/" rel="nofollow">CaRP for Rendering RSS Feeds in PHP</a></li>
-        <li><a href="http://www.webair.com/" rel="nofollow">Webair for Web Hosting</a></li>
-        <li><a href="http://www.widgetbucks.com/home.page?referrer=216001" rel="nofollow">WidgetBucks for Coupon Banners</a></li>
-        <li><a href="http://www.kontera.com/ads-for-site/become-a-kontera-publisher?type=1&aff_ID=22803" rel="nofollow">Kontera for Inline Advertising</a></li>
-        <li><a href="https://affiliate-program.amazon.com/" rel="nofollow">Amazon for Product Referrals</a></li>
-        <li><a href="http://chris.pirillo.com/free-clickbank-affiliate-rss-feeds/" rel="nofollow">ClickBank RSS for eBook Affiliates</a></li>
-        <li><a href="http://shoppingads.com/?r=94749a17de486147f2e4" rel="nofollow">ShoppingAds for eBay Banners</a></li>
-        <li><a href="http://tagjag.com/" rel="nofollow">TagJag for Generating Affiliate Links</a></li>
-        <li><a href="http://chris.pirillo.com/?s=adsense" rel="nofollow">Google AdSense for Monetizing Content</a></li>
-    </ul>
-    <h3>Follow Chris Pirillo through...</h3>
-    <ul>
-        <li><a href="http://l0ckergn0me.jaiku.com/" rel="nofollow">Jaiku</a></li>
-        <li><a href="http://www.youtube.com/lockergnome" rel="nofollow">YouTube</a></li>
-        <li><a href="http://chris.pirillo.com" rel="nofollow">His Blog</a></li>
-        <li><a href="http://live.pirillo.com/" rel="nofollow">Ustream.tv</a></li>
-        <li><a href="http://flickr.com/photos/lockergnome" rel="nofollow">Flickr</a></li>
-        <li><a href="http://twitter.com/chrispirillo" rel="nofollow">Twitter</a></li>
-        <li><a href="http://www.myspace.com/lockergnome" rel="nofollow">MySpace</a></li>
-        <li><a href="http://www.mybloglog.com/buzz/members/l0ckergn0me/" rel="nofollow">MyBlogLog</a></li>
-        <li><a href="http://www.facebook.com/people/Chris-Pirillo/680581396" rel="nofollow">FaceBook</a></li>
-        <li><a href="http://www.lockergnome.com/" rel="nofollow">Lockergnome</a></li>
-        <li><a href="http://friendfeed.com/l0ckergn0me" rel="nofollow">FriendFeed</a></li>
-        <li><a href="http://chris.pirillo.com/contact/" rel="nofollow">chris@pirillo.com</a></li>
-    </ul>
 </div>
