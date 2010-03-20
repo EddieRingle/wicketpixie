a9163b0c45f8f371f5715a0b1592f0d6330a6f55
Eddie Ringle <eddie@eringle.net>
2010-03-20 11:07:58 -0400

    Starting fresh. (#47)
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- app/advert.php
+++
@@ -1,8 +0,0 @@
-<div id="admin-affiliates">
-	<h3>Donate to WicketPixie</h3>					
-	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">  
-		<input type="hidden" name="cmd" value="_s-xclick"> 
-		<input type="hidden" name="hosted_button_id" value="2767322"> 
-		<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt=""> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-	</form>
-</div>
--- plugins/falbum/styles/default-ajax/exif.tpl.php
+++
@@ -1,8 +0,0 @@
-<table>
-	<?php foreach ($exif as $row): ?>	
-	<tr class='<?php echo $row['class']; ?>'>
-		<td><?php echo $row['label']; ?></td>
-		<td><?php echo $row['data']; ?></td>
-	</tr>
-	<?php endforeach; ?>
-</table>
