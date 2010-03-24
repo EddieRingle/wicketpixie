3edbb8d9a289fe697a04d028f122d6c3adc99a92
Eddie Ringle <eddie@eringle.net>
2010-03-23 20:12:09 -0400

    Clean up indentation
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- footer.php
+++ footer.php
@@ -1,33 +1,33 @@
 		</div>
-            </div>
-        </div>
-        <div id="footer-wrap">
-            <div id="footer">
-                <span class="footer-left">
-                    Copyright &copy; 2009 idlesoft labs
-                </span>
-                <span class="footer-right">
-                    Powered by <a href="http://chris.pirillo.com/wicketpixie">WicketPixie</a>.
-                </span>
-            </div>
-        </div>
-    </div>
+	</div>
+	</div>
+	<div id="footer-wrap">
+	<div id="footer">
+		<span class="footer-left">
+		Copyright &copy; 2009 idlesoft labs
+		</span>
+		<span class="footer-right">
+		Powered by <a href="http://chris.pirillo.com/wicketpixie">WicketPixie</a>.
+		</span>
+	</div>
+	</div>
+</div>
 <?php wp_footer(); ?>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script type="text/javascript" src="http://github.com/malsup/corner/raw/master/jquery.corner.js"></script>
 <script type="text/javascript">
-  $(document).ready(function() {
-    $("#body").corner("tr");
-    $("#footer-wrap").corner("bottom");
-    $("#page").corner("right bottom");
-    $("#sideline").corner();
-  });
+$(document).ready(function() {
+$("#body").corner("tr");
+$("#footer-wrap").corner("bottom");
+$("#page").corner("right bottom");
+$("#sideline").corner();
+});
 </script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/superfish.js"></script>
 <script type="text/javascript">
-  $(document).ready(function() {
-    $('ul.sf-menu').superfish();
-  });
+$(document).ready(function() {
+$('ul.sf-menu').superfish();
+});
 </script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -11,23 +11,23 @@
 ?>
 </head>
 <body>
-    <?php flush(); ?>
-    <div id="wrapper">
-        <div id="header-wrap">
-            <div id="header">
-                <div id="logo">
-                    <h1><a href="/">WicketPixie</a></h1>
-                </div>
-                <div id="sideline">
-                    <p><?php bloginfo('description'); ?></p>
-                </div>
-            </div>
-        </div>
-        <div id="body-wrap">
-            <div id="body">
-                <div id="navigation">
-                    <ul class="sf-menu">
-                        <?php
+<?php flush(); ?>
+<div id="wrapper">
+	<div id="header-wrap">
+	<div id="header">
+		<div id="logo">
+		<h1><a href="/">WicketPixie</a></h1>
+		</div>
+		<div id="sideline">
+		<p><?php bloginfo('description'); ?></p>
+		</div>
+	</div>
+	</div>
+	<div id="body-wrap">
+	<div id="body">
+		<div id="navigation">
+		<ul class="sf-menu">
+			<?php
 			if (!is_home()) {
 			?>
 			<li><a href="<?php bloginfo('home'); ?>">Home</a></li>
@@ -35,6 +35,6 @@
 			}
 			wp_list_pages('depth=3&sort_column=menu_order&title_li=');
 			?>
-                    </ul>
-                </div>
-                <div id="page">
+		</ul>
+		</div>
+		<div id="page">
