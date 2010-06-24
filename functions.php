0d297fec09b359f4921886c6feb423cb35b72d9e
Eddie Ringle <eddie@eringle.net>
2010-06-23 22:21:35 -0400

    Reorganize theme structure
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- functions.php
+++ functions.php
@@ -1,47 +1,5 @@
 <?php
 
-if ( function_exists('register_sidebar') ) {
-    register_sidebar(array('name'=>'sidebar_top',
-        'before_widget' => '<div id="%1$s" class="widget %2$s">',
-        'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));
-    register_sidebar(array('name'=>'sidebar1',
-	    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));	
-    register_sidebar(array('name'=>'sidebar2',		
-	    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));
-    register_sidebar(array('name'=>'sidebar3',		
-	    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));
-    register_sidebar(array('name'=>'sidebar4',		
-	    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));
-    register_sidebar(array('name'=>'sidebar5',		
-	    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));
-    register_sidebar(array('name'=>'sidebar6',		
-	    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</div>',
-	    'before_title' => '<h3>',
-	    'after_title' => '</h3>',
-    ));
-}
+require "framework/functions.php";
+
 ?>
