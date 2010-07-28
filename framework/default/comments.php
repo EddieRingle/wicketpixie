a4172ad23336a7e16a51b6462f9295f3d132d486
Eddie Ringle <eddie@eringle.net>
2010-07-28 12:09:57 -0400

    Use brackets for if else blocks
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- framework/default/comments.php
+++ framework/default/comments.php
@@ -31,11 +31,11 @@
 <!-- comments -->
 <div id="comments">
 
-<?php if ($comments) : ?>
+<?php if ($comments) { ?>
 
 	<h2><?php comments_number('No Comments', 'One Comment', '% Comments' );?></h2>
 
-	<?php foreach ($comments as $comment) : ?>
+	<?php foreach ($comments as $comment) { ?>
 
 	<!-- comment -->
 	<div class="comment" id="comment-<?php comment_ID() ?>">
@@ -50,9 +50,9 @@
 		<!-- comment content -->
 		<div class="content">
 			<?php echo get_avatar( get_comment_author_email(), $size = '36', $default = 'images/avatar.jpg' ); ?>
-			<?php if ($comment->comment_approved == '0') : ?>
+			<?php if ($comment->comment_approved == '0') { ?>
 			<p><em>Your comment is awaiting moderation.</em></p>
-			<?php endif; ?>
+			<?php } ?>
 			<?php comment_text() ?>
 		</div>
 		<!-- /comment content -->
@@ -60,26 +60,26 @@
 	</div>
 	<!-- /comment -->
 
-	<?php endforeach; /* end for each comment */ ?>
+	<?php } /* end for each comment */ ?>
 
- <?php else : // this is displayed if there are no comments so far ?>
+ <?php } else { // this is displayed if there are no comments so far ?>
 
-	<?php if ('open' == $post->comment_status) : ?>
+	<?php if ('open' == $post->comment_status) { ?>
 		<!-- If comments are open, but there are no comments. -->
 
-	 <?php else : // comments are closed ?>
+	 <?php } else { // comments are closed ?>
 		<!-- If comments are closed. -->
 		<p class="nocomments">Comments are closed.</p>
 
-	<?php endif; ?>
+	<?php } ?>
 
-<?php endif; ?>
+<?php } ?>
 
 </div>
 <!-- /comments -->
 
 
-<?php if ('open' == $post->comment_status) : ?>
+<?php if ('open' == $post->comment_status) { ?>
 
 	<!-- comment form -->
 	<div id="comment-form">
@@ -87,7 +87,7 @@
 	</div>
 	<!-- /comment form -->
 
-<?php endif; // If registration required and not logged in ?>
+<?php } // If registration required and not logged in ?>
 
-<?php endif; // if you delete this the sky will fall on your head ?>
+<?php } // if you delete this the sky will fall on your head ?>
 
