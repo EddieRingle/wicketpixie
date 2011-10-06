a744bf9cf3000bd9bfc22797444df9f9190f3807
Eddie Ringle <eddie@eringle.net>
2011-10-06 18:48:12 -0400

    Clean up whitespace in sidebar.php
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- framework/default/sidebar.php
+++ framework/default/sidebar.php
@@ -9,47 +9,36 @@
 ?>
 			<div id="sidebar" class="sidebar">
 			    <?php wipi_before_sidebar(); ?>
-				<!-- sidebar_top -->
+				        <!-- sidebar_top -->
                 <div class="sidebar_top">
                     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar_top') ) : ?><?php endif;?>
                 </div>
                 <!-- /sidebar_top -->
-
                 <!-- sidebar1 -->
                 <div class="sidebar1">
 	                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar1') ) : ?><?php endif; ?>
                 </div>
                 <!-- /sidebar1 -->
-
-
                 <!-- sidebar2 -->
                 <div class="sidebar2">	
 	                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar2') ) : ?><?php endif; ?>
                 </div>
                 <!-- /sidebar2 -->
-
-
                 <!-- sidebar3 -->
                 <div class="sidebar3">
 	                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar3') ) : ?><?php endif; ?>
                 </div>
                 <!-- /sidebar3 -->
-
-
                 <!-- sidebar4 -->
                 <div class="sidebar4">
 	                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar4') ) : ?><?php endif; ?>
                 </div>
                 <!-- /sidebar4 -->
-
-
                 <!-- sidebar5 -->
                 <div class="sidebar5">
 	                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar5') ) : ?><?php endif; ?>
                 </div>
                 <!-- /sidebar5 -->
-
-
                 <!-- sidebar6 -->
                 <div class="sidebar6">
 	                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar6') ) : ?><?php endif; ?>
